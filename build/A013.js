6a1d3128916dff7d372005ae9538a10297a14244
Rasquinha_ <brunodiasrasquinha@gmail.com>
2024-01-05 23:58:46 -0200

    classe e herança part2

--- build/A013.js
+++ build/A013.js
@@ -1,39 +1,73 @@
 "use strict";
 class Animal {
-    constructor(esp, gen, fome = false) {
+    constructor(esp, gen) {
         this.especie = esp;
         this.genero = gen;
-        this.fome = fome;
-    }
-    alimentar() {
-        let fome = this.fome ? false : false;
-        this.fome = fome;
-    }
-}
-const coelho = new Animal('coelho', 'M');
-class Pet extends Animal {
-    constructor(nome, nomedono, esp, gen, fome = false) {
-        super(esp, gen, fome); 
-        this.nome = nome;
-        this.nomeDono = nomedono;
+        this.nivelFome = this.setNivelDefome();
+        this.estaVivo = true;
         this.ID = this.setId();
-    }
-    infoPet() {
-        const pet = {
-            nome: this.nome,
-            dono: this.nomeDono,
-            id: this.ID,
-            mais: {
-                especie: this.especie,
-                genero: this.genero
-            }
-        };
-        console.log(pet);
+        this.loop = this.atualizarFome();
     }
     setId() {
         return crypto.randomUUID();
     }
+    setNivelDefome() {
+        return Number((Math.random() * 100).toFixed(0));
+    }
+    infoAnimal() {
+        let obj = {
+            id: this.ID,
+            especie: this.especie,
+            genero: this.genero,
+            nivel_de_fome: this.nivelFome,
+            status: this.estaVivo
+        };
+        return obj;
+    }
+    atualizarFome() {
+        return setInterval(() => {
+            this.nivelFome -= 5;
+            console.log(`nivel de fomo: ${this.nivelFome}%`);
+            if (this.nivelFome <= 30)
+                console.log('Animal com fome');
+            if (this.nivelFome <= 0)
+                this.animalMorto();
+        }, 10000);
+    }
+    alimentar() {
+        if (this.nivelFome <= 100 && this.nivelFome > 0) {
+            this.nivelFome += 50;
+            console.log(`hmmmmm.. Delicia!`);
+            console.log(`nivel de fomo: ${this.nivelFome}%`);
+        }
+    }
+    animalMorto() {
+        console.log(`${this.especie} está morto!`);
+        this.estaVivo = false;
+        clearInterval(this.loop);
+        console.log(this.infoAnimal());
+    }
 }
-const Osvaldo = new Pet('Osvaldo', 'Bruno', 'coelho', 'M', true);
-Osvaldo.infoPet();
-Osvaldo.alimentar();
+const coelho = new Animal('coelho', 'M');
+class Pet extends Animal {
+    constructor(esp, gen, nome, dono) {
+        super(esp, gen);
+        this.Nome = nome;
+        this.NomeDono = dono;
+    }
+    carinho() {
+        console.log(`Você fez carinho em ${this.Nome}`);
+    }
+    infoPet() {
+        const obj = {
+            Nome: this.Nome,
+            Dono: this.NomeDono,
+            Id: this.ID
+        };
+        console.log(obj);
+    }
+}
+const Osvaldo = new Pet('coelho', 'M', 'Osvaldo', 'Bruno');
+console.log(Osvaldo.infoAnimal());
+console.log(Osvaldo.infoPet());
+console.log(Osvaldo.carinho());
--- src/A013.ts
+++ src/A013.ts
@@ -5,65 +5,118 @@
 // classe pai
 class Animal {
 
-    especie: string
-    genero: 'M' | 'F'
-    fome: boolean
+    protected especie: string
+    protected genero: 'M' | 'F'
+    protected nivelFome: number
+    protected estaVivo: boolean
+    public ID: string
+    private loop;
 
-    constructor(esp: string, gen: 'M' | 'F', fome: boolean = false){
+    constructor(esp: string, gen: 'M' | 'F'){
+
         this.especie = esp
         this.genero = gen
-        this.fome = fome
-    }
-
-    alimentar(): void {
-        let fome = this.fome ? false : false
-        this.fome = fome
-    }
-}
-
-// instancia
-const coelho = new Animal('coelho', 'M')
-
-// classe herdeira
-class Pet extends Animal {
-
-    nome: string
-    nomeDono: string
-    ID: string 
-
-    constructor(nome: string, nomedono: string, esp: string, gen: 'M' | 'F', fome: boolean = false){
-
-        super(esp, gen, fome) // referenciaa classe pai
-
-        this.nome = nome
-        this.nomeDono = nomedono
+        this.nivelFome = this.setNivelDefome()
+        this.estaVivo = true
         this.ID = this.setId()
 
+        this.loop = this.atualizarFome()
+       
     }
 
-    infoPet(): void {
-        const pet = {
-            nome: this.nome,
-            dono: this.nomeDono,
-            id: this.ID,
-
-            mais: {
-                especie: this.especie,
-                genero: this.genero
-            }
-        }
-
-        console.log(pet)
-    }
-
-    setId(){
+    private setId(): string {
         return  crypto.randomUUID()
     }
-    
+
+    private setNivelDefome(): number{
+       return Number((Math.random() * 100).toFixed(0))
+    }
+
+    public infoAnimal() {
+
+        let obj = {
+            id: this.ID,
+            especie: this.especie,
+            genero: this.genero,
+            nivel_de_fome: this.nivelFome, 
+            status: this.estaVivo
+        }
+
+        return obj
+    }
+
+    private atualizarFome(){
+
+       return setInterval(() => {
+
+            this.nivelFome -= 5
+            console.log(`nivel de fomo: ${this.nivelFome}%`)
+
+            if(this.nivelFome <= 30) 
+                console.log('Animal com fome')
+
+            if(this.nivelFome <= 0) 
+                this.animalMorto()
+           
+        }, 10000)
+    }
+
+    public alimentar(): void {
+        if(this.nivelFome <= 100 && this.nivelFome > 0){
+            this.nivelFome += 50
+
+            console.log(`hmmmmm.. Delicia!`)
+            console.log(`nivel de fomo: ${this.nivelFome}%`)
+        }
+    }
+
+    private animalMorto() {
+        console.log(`${this.especie} está morto!`)
+        this.estaVivo = false
+        clearInterval(this.loop)
+        console.log(this.infoAnimal())
+    }
+
 }
 
-// instancia classe pet
-const Osvaldo = new Pet('Osvaldo', 'Bruno', 'coelho', 'M', true)
 
-Osvaldo.infoPet()
-Osvaldo.alimentar()
+// instancinado um animal
+const coelho = new Animal('coelho', 'M')
+
+
+//class filha
+class Pet extends Animal {
+
+    public Nome: string
+    public NomeDono : string
+
+    constructor(esp: string, gen: 'M' | 'F', nome: string, dono: string){
+
+        super(esp, gen)
+
+        this.Nome = nome
+        this.NomeDono = dono
+        
+    }
+
+    public carinho(): void {
+        console.log(`Você fez carinho em ${this.Nome}`)
+    }
+
+    public infoPet(): void {
+
+        const obj = {
+            Nome: this.Nome,
+            Dono: this.NomeDono,
+            Id: this.ID
+        }
+
+        console.log(obj)
+    }
+
+}
+
+const Osvaldo = new Pet('coelho', 'M', 'Osvaldo', 'Bruno')
+console.log(Osvaldo.infoAnimal())
+console.log(Osvaldo.infoPet())
+console.log(Osvaldo.carinho())
