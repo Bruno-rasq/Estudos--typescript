74a6ba278089c837bd8a40b11ddbf3b86a5f8fa2
Rasquinha_ <brunodiasrasquinha@gmail.com>
2024-01-06 23:55:36 -0200

    Globais e static

---
+++ src/A021.ts
@@ -0,0 +1,11 @@
+//---------------------------------------
+// typescript: Static
+//---------------------------------------
+
+import { Globais } from "./Globais";
+
+console.log(Globais.teste)
+
+Globais.teste = 10
+
+console.log(Globais.teste)
---
+++ src/Globais.ts
@@ -0,0 +1,10 @@
+export abstract class Globais {
+
+    public static teste: number = 0;
+    public static numbers: number[] = [10, 15, 20, 25]
+
+    public static Objeto = {
+        nome: 'Objeto Global',
+        tipo: 'objeto'
+    }
+}
